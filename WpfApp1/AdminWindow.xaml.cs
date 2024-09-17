a0c2a145e84b5e8b0d1fea1e5ee00135bd78a273
Földi Soma Vajk <foldi.soma.vajk@burg.lo.hu>
2024-09-17 13:34:25 +0200

    reszleges eloleges commit

--- WpfApp1/AdminWindow.xaml.cs
+++ WpfApp1/AdminWindow.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using WpfApp1.Adatmodellek;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for AdminWindow.xaml
-    /// </summary>
     public partial class AdminWindow : Window
     {
         public AdminWindow()
         {
             InitializeComponent();
+            LoadSubjectAverages();
+        }
+
+        private void LoadSubjectAverages()
+        {
+            SubjectAverageList.Items.Add("Matematika: 4.5");
+            SubjectAverageList.Items.Add("Történelem: 3.7");
+            SubjectAverageList.Items.Add("Fizika: 4.0");
+            SubjectAverageList.Items.Add("Informatika: 3.2");
         }
     }
 }
