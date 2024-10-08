cae5a5fb62fa30c7a53d5b50c50aef2ee98112e8
Varga Máté <vmate14538@gmail.com>
2024-10-08 13:30:09 +0200

    Mentés Js és Csv, Törles gomb elkezdve

--- ellenorzowpf2/MainWindow.xaml.cs
+++ ellenorzowpf2/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Specialized;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+
 
 namespace ellenorzowpf2
 {
@@ -27,16 +30,17 @@ namespace ellenorzowpf2
         }
 
         List<Tantargy> adatok = new List<Tantargy>();
+        List<Tantargy> AllData = new List<Tantargy>();
 
         public void Feladat1()
         {
+            
             string tantargyak = comboboxTan.Text;
             int evfolyam = Convert.ToInt32(comboboxOszt.Text);
             string kozvSzak = comboboxkozszak.Text;
             int hetioraszam = Convert.ToInt32(oraszamInput.Text);
             int evesoraszam =0;
             
-            Tantargy uj = new(tantargyak, evfolyam, kozvSzak, hetioraszam, evesoraszam);
 
                 if (evfolyam == 9 || evfolyam == 10 || evfolyam == 11)
                 {
@@ -58,29 +62,51 @@ namespace ellenorzowpf2
                 {
                     evesoraszam = hetioraszam * 31;
                 }
-            
+            Tantargy uj = new(tantargyak, evfolyam, kozvSzak, hetioraszam, evesoraszam);
             evesoszamLabel.Content = evesoraszam;
             adatok.Add(uj);
+            AllData.Add(uj);
         }
         public void Feladat2()
         {
             Feladat1();
             string json = JsonSerializer.Serialize(adatok);
-            File.WriteAllText(@"C:\Users\varga.mate\Documents\MATE\Projekt\elektronikus-ellen-rz-projekt-a-szaros-a-gatyasos\ellenorzowpf2\tantargy.json", json);
+            string file = System.IO.Directory.GetCurrentDirectory();
+            File.WriteAllText($@"{file}\tantargy.json", json, Encoding.UTF8);
+            adatok.Clear();
+            
         }
         public void Feladat3()
-        { 
-            
+        {
+            Feladat1();
+            string fileName = System.IO.Directory.GetCurrentDirectory();
+            using (StreamWriter sw = new($@"{fileName}\tantargy.csv", true, Encoding.UTF8))
+            {
+                sw.WriteLine($"{comboboxTan.Text}; {comboboxOszt.Text}; {comboboxkozszak.Text}; {oraszamInput.Text}; {evesoszamLabel.Content}");
+            }
+            adatok.Clear();
         }
         private void CsvMentes_Click(object sender, RoutedEventArgs e)
         {
-
+            Feladat3();
         }
 
         private void JsonMentes_Click(object sender, RoutedEventArgs e)
         {
             Feladat2();
         }
+        
+        private void JsTorles_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
 
+        private void CsvTorles_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            
+        }
     }
+    
 }
